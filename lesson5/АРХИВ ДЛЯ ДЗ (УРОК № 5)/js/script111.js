ed046904eaec94ad8f40a4c86bd739cf276cc253
kirillkolesnik123 <42231774+kirillkolesnik123@users.noreply.github.com>
2018-08-18 18:14:20 +0300

    Update script111.js

--- lesson5/АРХИВ ДЛЯ ДЗ (УРОК № 5)/js/script111.js
+++ lesson5/АРХИВ ДЛЯ ДЗ (УРОК № 5)/js/script111.js
@@ -10,7 +10,7 @@ let ly=document.getElementsByTagName('li');
 let changeli= ly[1].parentNode;
 changeli.insertBefore(ly[2],ly[1]);
 
-document.body.style.backgroundImage= "url('file:///C:/Users/kkolesnik/Desktop/Own/JS course/lesson5/АРХИВ ДЛЯ ДЗ (УРОК № 5)/img/apple_true.jpg')";
+document.body.style.backgroundImage="url('img/apple_true.jpg')";
 
 let changetitle= document.getElementById('title');
 changetitle.innerHTML = changetitle.innerHTML.replace('технику','подлинную технику');
