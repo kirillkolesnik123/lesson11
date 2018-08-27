ec1e3b1fdb9485b183e2ae2acec250e3e7b15073
kirillkolesnik123 <42231774+kirillkolesnik123@users.noreply.github.com>
2018-08-27 21:28:57 +0300

    Update scriptforlesson9.js

--- lesson9/simple/scriptforlesson9.js
+++ lesson9/simple/scriptforlesson9.js
@@ -1,48 +1,11 @@
-let arr1=[];
-function getFriendlyNumbers(start, end) {
-    let a = start;
-    let b = end;
-    let arr1_1=arr1;
- if(dr(start,end)===false){return []} else{
-        if (a <= 1 && b <= 1)
-        return []}
-    return arr1_1
-    
-    
-    
-
-
-    
-
-    
-console.log(arr1_1)
-}
- 
-
-
-function sumdel(number)
-{
-    let s=1;
-    let i=1;
-    for(i=2;i<=number/2;i++){
-     if( number %i==0) 
-        s+=i
-    }
-    return s
-}
-function dr(a,b){
-    let k
-    for(let j=a;j<=b;j++){
-        k=sumdel(j)
-        if(k>=a&&k<=b&&k>j){
-            if(sumdel(k)==j){
-                arr1.push([j,k])
-                
-            }
-        }
+let age = {
+    selector: document.getElementById('age'),
+    getValue() {
+        return this.selector.value
+    },
+    showUser(surname = 'Pupkin', name = 'Vasya', age = this.getValue()) {
+        return `User: ${surname} ${name}, his age are ${age}`
     }
 }
 
-document.write(getFriendlyNumbers());
-
-
+document.write(age.showUser());
