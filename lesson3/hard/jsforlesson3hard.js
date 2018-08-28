21d4f5fcb4af23d9bfb3ee8cf2469c0a7ec66c5a
kirillkolesnik123 <42231774+kirillkolesnik123@users.noreply.github.com>
2018-08-28 20:53:53 +0300

    Update jsforlesson3hard.js

--- lesson3/hard/jsforlesson3hard.js
+++ lesson3/hard/jsforlesson3hard.js
@@ -1,39 +1,40 @@
-//первое здаание
-/*let str ='Урок-3-был слишком лёгким'.toUpperCase();
-function replacer(str, offset, s) {
-      
-      return '  ';
-    }
-    
-    let result = str.replace(/-/gi, replacer);
-    console.log( result );
-    
-    let str2= str.slice(19,25); 
-    alert(str2);
-    let result2 = str2.replace(/им/gi, 'о');
-    console.log(result2.toUpperCase());
-*///второе задание
-/*let arr =[20,33,1,'Человек',2,3];
-let str=0;
 
-for( let i=0;i<arr.length;i++){
-	if(isFinite(arr[i])){
-		str=str+Math.pow(arr[i],3);
+let str = 'урок-3-был слишком легким';
+
+let say= (str.charAt(0).toUpperCase()+ str.substr(1).toLowerCase()).replace(/-/g, " ").substr(18).replace(/им/g, 'оо');
+
+console.log(say);
+
+
+arr = [20, 33, 1, 'Человек', 2, 3];
+
+let  radical = 0;
+
+for(i=0; i<arr.length; i++){
+	
+	if (isFinite(arr[i])) {
+		radical=radical + Math.pow(arr[i],3);
 	}
 }
 
-console.log(str);*/
-//третье задание
-function text(qwerty){
-	if(!isNaN(qwerty)){
-		console.log('Введите буквы!');
-	} else{
-	 qwerty = qwerty.trim();
-	 if(qwerty.length>=50){
-	 	qwerty=qwerty.substr(0,50)+'...';
+console.log(Math.sqrt(radical));
 
-	 }
+function showString(information){
+	
+	if (!isNaN(information)) {
+		console.log('Строку пиши');
+	}
 
-	} console.log(qwerty);
+	else {
+		information = information.trim();
+		if (information.length>=50) {
+			information = information.substr(0,50) + "...";
+		}
+	}
+	console.log(information);
+}
 
-}text('             sdfsdfsdfs');
+showString('             Привет Андрей')
+
+
+
